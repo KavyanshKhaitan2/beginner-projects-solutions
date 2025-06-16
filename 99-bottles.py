6cdd1ab33fc54a385553969c73f2b533f6342862
KavyanshKhaitan2 <kavyanshkhaitan11@gmail.com>
2025-06-16 16:43:54 +0530

    Added comments to the starting of the file, showing the task for 99 bottles.

--- 99-bottles.py
+++ 99-bottles.py
@@ -1,3 +1,18 @@
+# Solution by Kavyansh Khaitan (kavyansh.tech)
+
+#### 99 Bottles ####
+#
+# Create a program that prints out every line to the song "99 bottles of beer
+# on the wall."
+#
+# Tasks:
+# [-] Do not use a list for all of the numbers, and do not manually type them
+#     all in. Use a built in function instead.
+# [-] Besides the phrase "take one down," you may not type in any numbers/names
+#     of numbers directly into your song lyrics.
+# [-] Remember, when you reach 1 bottle left, the word "bottles" becomes singular.
+
+
 line = """
 'x' bottles of beer on the wall, 'x' bottles of beer.
 Take one down and pass it around, 'y' bottles of beer on the wall."""
