ec0a43aecbf7b661100d0c2c0ab56bb86803f30c
KavyanshKhaitan2 <kavyanshkhaitan11@gmail.com>
2025-06-19 19:58:15 +0530

    Added Mad Libs

---
+++ mad-libs.py
@@ -0,0 +1,46 @@
+# Solution by Kavyansh Khaitan (kavyansh.tech)
+
+#### Mad Libs Story Maker ####
+# Create a Mad Libs style game, where the program asks the user for certain
+# types of words, and then prints out a story with the words that the user
+# inputted.
+
+# [-] The story doesn't have to be too long, but it should have some sort of
+#     story line.
+# [-] Tip: it's easiest to write out a quick story on a piece of paper or a
+#     word document, and then go back through and see which words the user
+#     should be able to change.
+
+# Subgoals:
+# [-] If the user has to put in a name, change the first letter to a capital letter.
+# [-] Change the word "a" to "an" when the next word in the sentence begins with a vowel.
+
+
+print("\n"*50)
+print("##############################")
+print("#### Mad Libs Story Maker ####")
+print("##############################")
+print()
+print()
+place1 = input("Enter a noun: ")
+place2 = input("Enter a plural noun: ")
+place3 = input("Enter a verb ending in 'ing': ")
+place4 = input("Enter a plural noun: ")
+place5 = input("Enter a city name: ")
+place6 = input("Enter a plural noun: ")
+place7 = input("Enter an adjective: ")
+place8 = input("Enter a noun: ")
+place9 = input("Enter a number: ")
+place10 = input("Enter a noun: ")
+place11 = input("Enter an adjective: ")
+place12 = input("Enter a verb: ")
+place13 = input("Enter a verb: ")
+place14 = input("Enter a plural noun: ")
+place15 = input("Enter a verb ending in 'ing': ")
+place16 = input("Enter a number: ")
+place17 = input("Enter an adverb: ")
+place18 = input("Enter a noun: ")
+place19 = input("Enter an adjective: ")
+
+
+print(f"In 1981, the US launched the first real space {place1}. It was named Columbia and was piloted by two brave {place2}. They had practiced {place3} for two years and were expert {place4}. Columbia took off from {place5} using its powerful first-stage {place6} and soared off into the {place7} blue {place8}. At an altitude of {place9} feet, it went into orbit around the {place10}. For people watching from Earth, it was a/an {place11} sight to {place12}! Who could really {place13} that there were two {place14} in space? It was mind {place15}. After {place16} orbits, the shuttle landed {place17} at an air force {place18}. It was a/an {place19} day for the U.S. Space Program.")
